95077ef6ddeabe8a569a05b6295b74130f12599e
Emily A. Bellows <emily.a.bellows@gmail.com>
2016-01-04 20:09:23 -0600

    expanded the testing

--- main.cpp
+++ main.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include "hyperloglog.h"
+#include <vector>
 
 int main() {
-    emi::HyperLogLogCtx<std::string> ctx;
+    std::vector<std::string> words;
     std::ifstream ifs("/usr/share/dict/words");
     std::string input;
     while (ifs) {
         std::getline(ifs, input);
-        ctx.add(input);
+        words.push_back(input);
     }
-    auto distinct = ctx.finish();
-    std::cout << "Distinct: " << distinct << std::endl;
+
+    typedef std::vector<std::string>::iterator iter;
+    typedef std::hash<std::string> hash;
+
+#define TEST(n) \
+    {\
+        auto distinct = emi::HyperLogLog<iter, hash, (n)>(words.begin(), words.end());\
+        std::cout << (1<<(n)) << ": " << distinct;\
+        std::cout << " (error " << (std::abs(distinct - words.size()) / words.size()) * 100 << "%)";\
+        std::cout << std::endl;\
+    }\
+
+    TEST(1);
+    TEST(2);
+    TEST(3);
+    TEST(4);
+    TEST(5);
+    TEST(6);
+    TEST(7);
+    TEST(8);
+    TEST(9);
+    TEST(10);
+    TEST(11);
+    TEST(12);
     return 0;
 }
